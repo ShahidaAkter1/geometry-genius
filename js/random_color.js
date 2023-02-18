70bd9ddf75f669681bc736b16baefc53fcd8f8a1
ShahidaAkter1 <shahida0200@gmail.com>
2023-02-18 21:06:57 +0600

    random color generate update

--- js/random_color.js
+++ js/random_color.js
@@ -1,88 +1,85 @@
-// function randomColor() {
-//     let color = [];
-//     for (let i = 0; i < 3; i++) {
-//       color.push(Math.floor(Math.random() * 256));
-//     }
-//     return 'rgb(' + color.join(', ') + ')';
-//   } 
-  
-//   document.getElementById('color-1').addEventListener("mouseover", function(e){
-//    e.target.style.backgroundColor = randomColor();
-//   });
-//   document.getElementById('color-2').addEventListener("mouseover", function(e){
-//    e.target.style.backgroundColor = randomColor();
-//   });
-//   document.getElementById('color-3').addEventListener("mouseover", function(e){
-//    e.target.style.backgroundColor = randomColor();
-//   });
-//   document.getElementById('color-4').addEventListener("mouseover", function(e){
-//    e.target.style.backgroundColor = randomColor();
-//   });
-//   document.getElementById('color-5').addEventListener("mouseover", function(e){
-//    e.target.style.backgroundColor = randomColor();
-//   });
-//   document.getElementById('color-6').addEventListener("mouseover", function(e){
-//    e.target.style.backgroundColor = randomColor();
-//   });
-
-
-//   document.getElementById('color-2').addEventListener("mouseover", function(){
-//     document.body.style.backgroundColor = randomColor();
-//   });
-//   document.getElementById('color-3').addEventListener("mouseover", function(){
-//     document.body.style.backgroundColor = randomColor();
-//   });
-  
-
-//   document.querySelector('color-1').addEventListener('mouseover', event => {
-//         event.target.style.fill = randomColor();
-//   });
-
-// ================
-// $(function(){
-//     var $container = $('#container');
-
-//     $container.mousemove(function(){
-//       var rgb = [
-//         parseInt(Math.random() * 255),
-//         parseInt(Math.random() * 255),
-//         parseInt(Math.random() * 255)
-//       ]; // generating array of Red Green Blue numbers (will be used to change background color)
-//       $container
-//         .css('background-color', 'rgb(' + rgb.join(',') + ')')
-//         .css('color', 'rgb(' + [255-rgb[0], 255-rgb[1], 255-rgb[2]].join(',') + ')');
-//     });
-//   });
-
-//   =========================
 function randomColor() {
-  let color = [];
-  for (let i = 0; i < 3; i++) {
-    color.push(Math.floor(Math.random() * 256));
+    let color = [];
+    for (let i = 0; i < 3; i++) {
+      color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
+  } 
+  
+  
+  function passId(id){
+    const get=document.getElementById(id);
+    get.style.backgroundColor=randomColor();
+    get.style.border='1px solid blue';
+  
   }
-  return 'rgb(' + color.join(', ') + ')';
-} 
-
-document.addEventListener("mouseover", function(){
-  document.body.style.backgroundColor = randomColor();
-});
-
-
-document.addEventListener("mouseout", function(){
-    document.body.style.backgroundColor = 'none';
-  });
-// ====================
-// const cards=document.getElementById('container');
-// for(const card of cards){
-//     card.addEventListener('mouseover',function(){
-//         let color = [];
-//   for (let i = 0; i < 3; i++) {
-//     color.push(Math.floor(Math.random() * 256));
-//   }
- 
-// } 
-//     })
-//     return 'rgb(' + color.join(', ') + ')';
-// }
-
- 
+  
+  
+  //1
+  document.getElementById('color-1').addEventListener('mouseover',function(){
+    const first=passId('color-1');
+  })
+  
+  document.getElementById('color-1').addEventListener('mouseout',function(){
+    const getId=document.getElementById('color-1');
+    getId.style.backgroundColor='white';
+    getId.style.border='none';
+  })
+  
+  
+  //2
+  document.getElementById('color-2').addEventListener('mouseover',function(){
+    const first=passId('color-2');
+  })
+  
+  document.getElementById('color-2').addEventListener('mouseout',function(){
+    const getId=document.getElementById('color-2');
+    getId.style.backgroundColor='white';
+    getId.style.border='none';
+  })
+  
+  //3
+  document.getElementById('color-3').addEventListener('mouseover',function(){
+    const first=passId('color-3');
+  })
+  
+  document.getElementById('color-3').addEventListener('mouseout',function(){
+    const getId=document.getElementById('color-3');
+    getId.style.backgroundColor='white';
+    getId.style.border='none';
+  })
+  
+  
+  //4
+  document.getElementById('color-4').addEventListener('mouseover',function(){
+    const first=passId('color-4');
+  })
+  
+  document.getElementById('color-4').addEventListener('mouseout',function(){
+    const getId=document.getElementById('color-4');
+    getId.style.backgroundColor='white';
+    getId.style.border='none';
+  })
+  
+  //5
+  document.getElementById('color-5').addEventListener('mouseover',function(){
+    const first=passId('color-5');
+  })
+  
+  document.getElementById('color-5').addEventListener('mouseout',function(){
+    const getId=document.getElementById('color-5');
+    getId.style.backgroundColor='white';
+    getId.style.border='none';
+  })
+  
+  
+  //6
+  document.getElementById('color-6').addEventListener('mouseover',function(){
+    const first=passId('color-6');
+  })
+  
+  document.getElementById('color-6').addEventListener('mouseout',function(){
+    const getId=document.getElementById('color-6');
+    getId.style.backgroundColor='white';
+    getId.style.border='none';
+  })
